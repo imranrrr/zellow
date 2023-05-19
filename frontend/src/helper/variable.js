92803a3e989aa1e51f9cb7ead61dca005f12b720
Bilal Choudhry <bilalachoudhry@gmail.com>
2023-05-19 11:46:49 -0700

    update BASE_URL

--- frontend/src/helper/variable.js
+++ frontend/src/helper/variable.js
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ?
-"https://zillion.onrender.com" :
-'http://localhost:3000';
+export const BASE_URL = (process.env.NODE_ENV !== 'development') ?
+    ("https://zillion.onrender.com") :
+    ('http://localhost:3000');
