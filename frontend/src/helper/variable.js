dcc93d1bf3450b404cd03a50cebfeaa466f61285
Bilal Choudhry <bilalachoudhry@gmail.com>
2023-06-05 12:52:43 -0700

    deployment

--- frontend/src/helper/variable.js
+++ frontend/src/helper/variable.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://0.0.0.0:10000"
+export const BASE_URL = (process.env.NODE_ENV !== 'development') ?
+    ("https://zillion.onrender.com") :
+    ('http://localhost:3000');
